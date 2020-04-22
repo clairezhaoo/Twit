f13d202ba3ae5e76c6b7f2768c3c1f1c5541a56f
clairezhaoo <62403358+clairezhaoo@users.noreply.github.com>
2020-04-22 10:58:25 -0700

    Update models.py

--- twit/twitter/models.py
+++ twit/twitter/models.py
@@ -19,3 +19,14 @@ class Tweet(models.Model):
 
     def get_absolute_url(self):
         return reverse('twitter:detail', kwargs={'pk': self.pk})
+
+"""
+class Reply(models.Model):
+    reply_text = models.CharField(max_length=250)
+    pub_date = models.DateTimeField(auto_now_add=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return self.reply_text
+
+
+"""
